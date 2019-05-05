3d7952801a027820272e335dd4f4c76cd4b7b311
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2019-05-05 10:33:56 -0300

    feature(Content): finaliza parte basica do conteudo principal

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardContent,
+  CardHeader,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>Compra de R$6,00 em pagseguro do brasil</Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
       <Tabs />
     </Container>
   );
